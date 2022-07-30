6e171c75bb32c0becb3b30d685613929d6abab1f
Cersei2005 <1332393799@qq.com>
2022-07-30 19:48:35 +0800

    项目初始化目录 配置postcss-pxtorem和lib-flexible进行rem适配

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './styles/index.less'
 import router from './router'
 import store from './store'
-
+import 'amfe-flexible'
 createApp(App).use(store).use(router).mount('#app')
